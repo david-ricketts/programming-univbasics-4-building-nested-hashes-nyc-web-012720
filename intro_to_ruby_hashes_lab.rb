8f640d7b0837ccb88e07a8a6f4b63664f86abdfe
David Ricketts <david.ricketts119@gmail.com>
2020-01-08 04:53:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = { :railroads => { 
     :pieces => 4,
-    :rent_in_dollars => "nil":,
+    :rent_in_dollars => "nil",
   } }
 	base_hash
 end
