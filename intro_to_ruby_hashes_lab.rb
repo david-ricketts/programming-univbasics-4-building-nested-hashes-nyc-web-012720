d80dcffba4a58f143c35075fda3e044778a4edb1
David Ricketts <david.ricketts119@gmail.com>
2020-01-08 05:10:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
     :pieces => 4,
     :rent_in_dollars => {
      "key1" => "",
-     key2 => "",
-     key3 => "",
+     "key2" => "",
+     "key3" => "",
      "key4" => ""
     }
     }
